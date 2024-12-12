51cc6542bd7e4200c6f5344bcf732c150e9e213e
bgreenbones <gwoodben@gmail.com>
2024-12-12 16:14:19 -0500

    should be ready to go

--- src/pages/Tech.js
+++ src/pages/Tech.js
@@ -35,24 +35,44 @@ function getEventElement(event) {
 function PastProjects() { return pastEvents.map(event => getEventElement(event)) };
 function UpcomingProjects() { return upcomingEvents.map(event => getEventElement(event)) };
 
-const Tech = () => <div id="projectsBackground">
-                        <div id="projectsOverlay">
-                          <h2>projects</h2>
-                          <h3>music generator</h3>
-                          My foray into algorithmic composition, conveniently embedded in a DAW plugin.
-                          <p/>Today, generative AI allows us to generate stuff that sounds like anything we've heard before.
-                          <p/>My generator outputs harmonies, rhythms, melodies, and basslines based on abstract concepts that I've manually coded up, instead of training on millions/billions of examples.
-                          <p/>Therefore, it's extremely efficient and free of copyright issues, and it outputs weird stuff that simultaneously sounds like nothing I've heard before, and seems oddly close to the music swimming around in my subconcious brain.
-                          <p/>
-                          <img style={{maxWidth: "100%", borderRadius: "10px"}} src="../media/projects/generateStuffScreenshot.png"></img>
-                          <div style={{fontSize: 12}}>Pictured: a beautiful UI that makes it clear I am trying to make tons of money off this thing.</div>
-                          <p/>Some stuff this thing has spat out (sounds could be whatever synths you want, as long as you can get MIDI signals into them):
-                          
-                          <div id="projectsGrid">
-                            {/* <div className="projects" style={{paddingRight:"1rem"}}><h3 style={{textAlign:"start"}}>Upcoming Projects</h3>{UpcomingProjects()}</div>
-                            <div className="projects"><h3 style={{textAlign:"start"}}>Past Projects</h3>{PastProjects()}</div> */}
-                          </div>
-                        </div>
-                      </div>;
+const Tech = () => (
+<div id="projectsBackground">
+  <div id="projectsOverlay">
+    <h2>projects</h2>
+    <h3>music generator</h3>
+    My foray into algorithmic composition, conveniently embedded in a DAW plugin.
+    <p/>Today, generative AI allows us to generate stuff that sounds like anything we've heard before.
+    <p/>My generator outputs harmonies, rhythms, melodies, and basslines based on abstract concepts that I've manually coded up, instead of training on millions/billions of examples.
+    <p/>Therefore, it's extremely efficient, free of copyright issues, and it outputs weird stuff that sounds like nothing I've heard before but is oddly close to the music swimming around in my subconcious brain.
+    <p/>
+    <img style={{maxWidth: "100%", borderRadius: "10px"}} src="../media/projects/generateStuffScreenshot.png"></img>
+    <div style={{fontSize: 12}}>Pictured: a beautiful UI that makes it clear I am trying to make tons of money off this thing.</div>
+    <p/>Some stuff this thing has spat out (sounds could be whatever synths you want, as long as you can get MIDI signals into them):
+    <p/><audio controls>
+      <source src="../media/projects/music/pluginDups.mp3"></source>
+    </audio>
+    <p/>By changing the subdivision selection you can output stuff in triplet time (kick drum added for downbeat reference):
+    <p/><audio controls>
+      <source src="../media/projects/music/pluginTrips2.mp3"></source>
+    </audio>
+    <p/>Or even quintuplets:
+    <p/><audio controls>
+      <source src="../media/projects/music/pluginQuints.mp3"></source>
+    </audio>
+    <p/>As a drummer, this makes for fun stuff to play along to. You can download the current build's .VST3 file <a href="../media/projects/generateStuff.zip">here</a>, but it probably won't work for you unless you're running a Mac with an M1 chip (again, I am trying to get rich from this). If you want to try building from source, it is available on <a href="https://github.com/bgreenbones/generateStuff/tree/main">github</a>.
+    <h3>sticking generator</h3>
+    Sometimes when I practice the drums, I want to get away from my habits. This app is a dead simple text generator that can spit out ideas that I haven't thought to try.
+    <p/>Say I want to keep my hi-hat foot playing your typical waltz pattern, while my left hand and right foot do different things in the spaces between. These settings:
+    <p/><img style={{maxWidth: "100%", borderRadius: "10px", marginLeft: "2px"}} src="../media/projects/stickings2.jpg"></img>
+    <p/>Will give me just that!
+    <p/><img style={{maxWidth: "100%", borderRadius: "10px", marginRight: "2px"}} src="../media/projects/stickings1.jpg"></img>
+    <p/>I haven't released or deployed this anywhere (because I'm trying to make money from it), but you could build it from the source available on <a href="https://github.com/bgreenbones/drumPracticeHelper">github</a>. It is built using Flutter, which can get you up and running desktop, mobile, or web.
+    
+    <div id="projectsGrid">
+      {/* <div className="projects" style={{paddingRight:"1rem"}}><h3 style={{textAlign:"start"}}>Upcoming Projects</h3>{UpcomingProjects()}</div>
+      <div className="projects"><h3 style={{textAlign:"start"}}>Past Projects</h3>{PastProjects()}</div> */}
+    </div>
+  </div>
+</div>);
 
 export default Tech;
